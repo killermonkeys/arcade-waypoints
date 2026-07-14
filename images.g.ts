d29b1832dda2c8ff5a1e52abbedb5e1e07dd1b25
killermonkeys <kstoltz@dubious.biz>
2026-07-14 18:34:04 +0100

    Update tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts, test.ts

--- images.g.ts
+++ images.g.ts
@@ -3,25 +3,7 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "image1":
-            case "carRedRight":return img`
-. . . . . . . . . . . . . . . . 
-. . . . 2 2 2 2 2 2 2 2 . . . . 
-. . . 2 4 2 2 2 2 2 2 c 2 . . . 
-. . 2 c 4 2 2 2 2 2 2 c c 2 . . 
-. 2 c c 4 4 4 4 4 4 2 c c 4 2 d 
-. 2 c 2 e e e e e e e b c 4 2 2 
-. 2 2 e b b e b b b e e b 4 2 2 
-. 2 e b b b e b b b b e 2 2 2 2 
-. e e 2 2 2 e 2 2 2 2 2 e 2 2 2 
-. e e e e e e f e e e f e 2 d d 
-. e e e e e e f e e f e e e 2 d 
-. e e e e e e f f f e e e e e e 
-. e f f f f e e e e f f f e e e 
-. . f f f f f e e f f f f f e . 
-. . . f f f . . . . f f f f . . 
-. . . . . . . . . . . . . . . . 
-`;
+
         }
         return null;
     })
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -2,103 +2,10 @@
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile1 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile2 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile3 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile4 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile6 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile7 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile8 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile9 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile10 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile11 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile12 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile13 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile14 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile5 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile15 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile16 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile17 = image.ofBuffer(hex``);
-
-    helpers._registerFactory("tilemap", function(name: string) {
-        switch(helpers.stringTrim(name)) {
-            case "map":
-            case "map1":return tiles.createTilemap(hex`1000100004040404040404040404040404040404040404040404040404040404040404040404040101030101070101010104040404040101010101010101010301010404040103040404040404040401010104040401010404010101080101030104040404010104010501010101010104040404040201040101040404040404040404040401010401050101010108010101040404010104040101010101010101050104040101040404040404040404040101040401060104040404040404040401010404010106010101040404040404010104040101010101010106010101010501040404010101010101010101010101010404040404040404040404040404040404`, img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16,sprites.skillmap.islandTile4,sprites.vehicle.roadTurn1,sprites.vehicle.roadTurn2,sprites.dungeon.hazardLava0,sprites.vehicle.roadTurn3,sprites.vehicle.roadTurn4,myTiles.tile2,myTiles.tile3], TileScale.Sixteen);
-        }
-        return null;
-    })
 
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
-            case "roadTurn1":
-            case "tile1":return tile1;
-            case "roadTurn2":
-            case "tile2":return tile2;
-            case "roadTurn3":
-            case "tile3":return tile3;
-            case "roadTurn4":
-            case "tile4":return tile4;
-            case "number02":
-            case "tile6":return tile6;
-            case "number03":
-            case "tile7":return tile7;
-            case "number04":
-            case "tile8":return tile8;
-            case "number05":
-            case "tile9":return tile9;
-            case "number06":
-            case "tile10":return tile10;
-            case "number07":
-            case "tile11":return tile11;
-            case "number08":
-            case "tile12":return tile12;
-            case "number09":
-            case "tile13":return tile13;
-            case "number01":
-            case "tile14":return tile14;
-            case "number11":
-            case "tile5":return tile5;
-            case "number10":
-            case "tile15":return tile15;
-            case "number12":
-            case "tile16":return tile16;
-            case "number00":
-            case "tile17":return tile17;
         }
         return null;
     })
